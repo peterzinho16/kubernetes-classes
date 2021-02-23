8f6c6d88f3932c6307143daac7f7c64806911713
peterzinho16 <peterpaul.0194@gmail.com>
2021-02-22 23:06:37 -0500

    hpa files

--- podgateway/src/main/java/com/bindord/podgateway/controller/PublicController.java
+++ podgateway/src/main/java/com/bindord/podgateway/controller/PublicController.java
@@ -1,11 +1,19 @@
 package com.bindord.podgateway.controller;
 
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import javax.net.ssl.SSLException;
 
 @RestController
 @RequestMapping("/public")
@@ -17,8 +25,15 @@ public class PublicController {
     private String idSvcHost;
 
     @GetMapping("/get/id")
-    public Mono<String> generateId() {
+    public Mono<String> generateId() throws SSLException {
+        SslContext sslContext = SslContextBuilder
+                .forClient()
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .build();
+        HttpClient httpClient = HttpClient.create().secure(t -> t.sslContext(sslContext));
+
         return WebClient.builder().baseUrl(idSvcHost + GEN_ID_ENDPOINT)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .build()
                 .get()
                 .retrieve().bodyToMono(String.class);
